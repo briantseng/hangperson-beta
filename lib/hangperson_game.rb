ce6d47a19ee71b86bfcfff06409b6722ce45db05
Brian Tseng <briantseng@berkeley.edu>
2015-02-06 01:54:53 -0400

    Passed first test block

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,6 +5,14 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
+  attr_accessor :word, :guesses, :wrong_guesses
+
+  def initialize(new_word)
+    @word = new_word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
