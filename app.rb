dc5c461912f0d0772ca9b1887ec8d41a57768f68
Brian Tseng <briantseng@berkeley.edu>
2015-02-08 21:10:07 -0400

    Invalid and repeated guesses.

--- app.rb
+++ app.rb
@@ -40,8 +40,17 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    @game.guess(letter)
-    redirect '/show'
+    if @game.guesses.include? letter or @game.wrong_guesses.include? letter
+      flash[:message] = 'You have already used that letter'
+      redirect '/show'
+    else
+      begin
+        @game.guess(letter)
+      rescue
+        flash[:message] = 'Invalid guess.'
+      end
+      redirect '/show'
+    end
   end
   
   # Everytime a guess is made, we should eventually end up at this route.
